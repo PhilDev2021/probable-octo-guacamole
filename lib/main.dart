84b660bd95fdb065f94658865578afd0349d12c2
CodePhilanthropist <barrigaete@gmail.com>
2021-10-23 20:00:42 +0800

    add home

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
+      home: Home(),
     ),
   );
 }
