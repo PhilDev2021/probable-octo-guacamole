22bbe2f9502880c84b9b35e1d19805177727c89f
CodePhilanthropist <barrigaete@gmail.com>
2021-10-23 20:02:22 +0800

    experimenting with stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.blue,
       body: Container(),
     );
   }
