0667bf602b74cdf53c0d862df1a71234c2b21103
CodePhilanthropist <barrigaete@gmail.com>
2021-10-25 04:19:41 +0800

    add fontweifht

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,7 @@ class Home extends StatelessWidget {
                       email,
                       style: TextStyle(
                         fontSize: 20,
+                        letterSpacing: 2.5,
                         color: Colors.teal[500],
                       ),
                     ),
